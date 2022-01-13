3b215c8f91d30c147ead225f06e4920ef62e08dd
pom1dorka <zed3008@gmail.com>
2022-01-14 02:33:29 +0300

    perfect RuleViewer and PlayerInputField

--- rules_screen.py
+++ rules_screen.py
@@ -2,13 +2,10 @@ import os
 import pygame
 import pygame_widgets
 from pygame_widgets.button import Button
-from pygwidgets import DisplayText
+from pygwidgets import DisplayText, InputText, CustomCheckBox
 
 from load_image import load_image
 from terminate import terminate
-
-
-first_draw = 0
 
 
 class RuleViewer:
@@ -77,37 +74,107 @@ class RuleViewer:
         self.rule_name_disp.setLoc(self.r_n_d_loc)
 
 
+class PlayerInputField:
+    def __init__(self, screen, x, y, color, placeholder):
+        screen.fill('white', (x, y, 410, 50))
+        screen.fill(color, (x, y, 50, 50))
+        pygame.draw.rect(screen, 'black', (x, y, 410, 50), width=3)
+        pygame.draw.rect(screen, 'black', (x, y, 361, 50), width=3)
+        pygame.draw.rect(screen, 'black', (x, y, 50, 50), width=3)
+
+        self.name_inp = InputText(screen, (x + 56, y + 15), value=placeholder, width=277,
+                                  fontName="./data/UpheavalPro.ttf", fontSize=40, focusColor='white')
+        self.disabled_name_inp = InputText(screen, (x + 56, y + 15), value=placeholder, width=277,
+                                           fontName="./data/UpheavalPro.ttf", fontSize=40, focusColor='white',
+                                           textColor='grey')
+        self.name_inp.disable()
+        self.disabled_name_inp.show()
+        self.check = CustomCheckBox(screen, (x + 362, y + 2), on='data/checked.png', off='data/unchecked.png',
+                                    callBack=lambda n=1: self.check_toggled())
+
+    def check_toggled(self):
+        if self.check.getValue():
+            self.disabled_name_inp.setValue(self.name_inp.getValue())
+            self.name_inp.disable()
+            self.disabled_name_inp.show()
+        if not self.check.getValue():
+            self.name_inp.enable()
+            self.disabled_name_inp.hide()
+
+
+
+
 def rules_screen(screen, WIDTH=1280, HEIGHT=720):
     fon = pygame.transform.scale(load_image('./data/fon_main.jpg'), (WIDTH, HEIGHT))
     screen.fill((0, 0, 0))
     screen.blit(fon, (0, 0))
 
-
     color = [(0, 0, 0), (75, 0, 130), (128, 0, 128)]
     for i in range(3):
-        choose_rules_label = DisplayText(screen, (55 - 1 * i, 22 - 1 * i),
+        choose_rules_label = DisplayText(screen, (70 - 1 * i, 32 - 1 * i),
                                          fontName="./data/UpheavalPro.ttf", fontSize=40,
                                          value='Выберите\nправила игры:',
                                          justified='center', textColor=color[i])
 
         choose_rules_label.draw()
-        choose_rules_label = DisplayText(screen, (55 - 1 * i + 865, 22 - 1 * i),
+        choose_players_label = DisplayText(screen, (70 - 1 * i + 420, 22 - 1 * i),
                                          fontName="./data/UpheavalPro.ttf", fontSize=40,
-                                         value='Выберите\nправила\nдля сплава',
+                                         value='Введите\nимена игроков:',
+                                         justified='center', textColor=color[i])
+
+        choose_players_label.draw()
+        choose_rules_label = DisplayText(screen, (90 - 1 * i + 865, 22 - 1 * i),
+                                         fontName="./data/UpheavalPro.ttf", fontSize=40,
+                                         value='Выберите\nправила\nдля сплава:',
                                          justified='center', textColor=color[i])
 
         choose_rules_label.draw()
 
     game_rules = RuleViewer(screen, 15, 105, 'pravila')
     splav_rules = RuleViewer(screen, 865, 105, 'dla_splav')
-    text = [('Выберите', 'правила игры:'), ('123',)]
+
+    color = ['red', 'yellow', (17, 113, 209), (23, 163, 5)]
+    fields = []
+    for i in range(4):
+        field = PlayerInputField(screen, 435, 106 + 65 * i, color[i], f'Игрок{i + 1}')
+        fields.append(field)
+#       старый код, до переноса в класс
+        '''screen.fill('white', (435, 106 + 65 * i, 410, 50))
+        screen.fill(color[i], (435, 106 + 65 * i, 50, 50))
+        pygame.draw.rect(screen, 'black', (435, 106 + 65 * i, 410, 50), width=3)
+        pygame.draw.rect(screen, 'black', (435, 106 + 65 * i, 361, 50), width=3)
+        pygame.draw.rect(screen, 'black', (435, 106 + 65 * i, 50, 50), width=3)
+
+        name_inp = InputText(screen, (491, 121 + 65 * i), value=f'Игрок{i+1}', width=277,
+                             fontName="./data/UpheavalPro.ttf", fontSize=40, focusColor='black')
+        player_names.append(name_inp)
+        check = CustomCheckBox(screen, (797, 108 + 65 * i), on='data/checked.png', off='data/unchecked.png',
+                               nickname=f'{i+1}')
+        checks.append(check)'''
+    for i in fields:
+        if fields.index(i) < 2:
+            i.check_toggled()
+            i.check.setValue(1)
+
     while True:
         events = pygame.event.get()
         for event in events:
             if event.type == pygame.QUIT:
                 terminate()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                print(event.pos)
+            '''if p1.handleEvent(event) or p2.handleEvent(event) or p3.handleEvent(event) or p4.handleEvent(event):
+                pass
+            if ch1.handleEvent(event) or ch2.handleEvent(event) or ch3.handleEvent(event) or ch4.handleEvent(event):
+                print()'''
+            for i in fields:
+                if i.name_inp.handleEvent(event) or i.check.handleEvent(event):
+                    pass
 
-
+        for i in fields:
+            i.name_inp.draw()
+            i.check.draw()
+            i.disabled_name_inp.draw()
         pygame.display.flip()
         pygame_widgets.update(events)
         pygame.display.update()
