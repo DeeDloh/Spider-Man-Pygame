d12c8cb77d6060fce17626d32042b0dcc543b994
pom1dorka <zed3008@gmail.com>
2022-01-22 05:52:47 +0300

    100% perfect leaderboard EZ

--- leaderboard.py
+++ leaderboard.py
@@ -24,18 +24,25 @@ class LeadTable:
         self.slider = Slider(screen, 1224, 40, 20, 640, vertical=True, step=40, handleRadius=20,
                              colour=(142, 68, 173), handleColour=(91, 44, 111))
         self.no_slider_flag = False
-        if self.lb_len < 17:
+        if self.lb_len < 16:
             self.slider.disable()
             self.slider._hidden = True
             self.no_slider_flag = True
-        self.slider.max = 40 * (self.lb_len - 16)
+        self.slider.max = 40 * (self.lb_len - 15)
         self.slider.setValue(self.slider.max)
         self.slider._hidden = True
 
-        self.button_1 = SpiderButton(screen, (0, 0), '<-', width=150, height=60, upColor=(187, 143, 206),
+        self.button_1 = SpiderButton(screen, (15, 15), '<-', width=150, height=60, upColor=(187, 143, 206),
                                      overColor=(165, 105, 189), downColor=(125, 60, 152),
-                                     fontName="./data/UpheavalPro.ttf", fontSize=40)
+                                     fontName="./data/UpheavalPro.ttf", fontSize=40, textColor='white')
         self.button_1.hide()
+
+        self.button_inf = SpiderButton(screen, (15, 645), 'info', width=150, height=60, upColor=(143, 144, 206),
+                                       overColor=(105, 105, 189), downColor=(105, 105, 189),
+                                       fontName="./data/UpheavalPro.ttf", fontSize=40, textColor='white')
+        self.button_inf.hide()
+        self.info_field_back = pygame.Surface((330, 100), pygame.SRCALPHA)
+        self.info_field_back.fill((202, 93, 249, 152))
 
         self.click_back = False
         self.scroll(0)
@@ -47,16 +54,18 @@ class LeadTable:
     def disabled_button(self):
         self.slider._hidden = True
         self.button_1.hide()
+        self.button_inf.hide()
 
     def enabled_button(self):
         if not self.no_slider_flag:
             self.slider._hidden = False
         self.button_1.show()
+        self.button_inf.show()
 
     def scroll(self, movement):
         x = 400
         for i in range(2):
-            y = 40 + (movement - self.slider.max)
+            y = 90 + (movement - self.slider.max)
             for j in range(self.lb_len):
                 y += 10
                 if i == 0:
@@ -73,12 +82,13 @@ class LeadTable:
             self.click_back = False
             self.disabled_button()
             return 1
+
         sl = self.slider.getValue()
         sl_max = self.slider.max
-
         for event in events:
             if self.button_1.handleEvent(event):
                 self.clicked_back()
+            self.button_inf.handleEvent(event)
             if event.type == pygame.MOUSEBUTTONDOWN and not self.no_slider_flag:
                 if event.button == 1:
                     print(event.pos)
@@ -92,10 +102,20 @@ class LeadTable:
                         self.slider.setValue(sl - 40)
                     else:
                         self.slider.setValue(0)
+
         self.scroll(sl)
-        self.screen.blit(self.fon, (350, 0), (350, 0, 580, 40))
-        self.screen.blit(self.fon, (350, 680), (350, 680, 580, 40))
+        self.screen.blit(self.fon, (350, 0), (350, 0, 580, 80))
+        self.screen.blit(self.fon, (350, 690), (350, 690, 580, 40))
+        self.screen.blit(self.font.render('имя', True, 'white'), (400, 20))
+        self.screen.blit(self.font.render('очки', True, 'white'), (784, 20))
         self.button_1.draw()
+        self.button_inf.draw()
+        if pygame.mouse.get_pos()[0] in range(15, 165) and pygame.mouse.get_pos()[1] in range(645, 705):
+            self.info_field_back.blit(self.font.render('победа +30', True, 'white'), (20, 20))
+            self.info_field_back.blit(self.font.render('поражение -15', True, 'white'), (20, 60))
+            self.screen.blit(self.info_field_back, (15, 530), (0, 0, 330, 500))
+        else:
+            self.screen.blit(self.fon, (15, 530), (15, 530, 330, 100))
         pygame_widgets.update(events)
 
 
